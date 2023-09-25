5c574a92b6ca6a181fbe5ee7138ab90582ba9991
nanewtech <itznanewtech@gmail.com>
2023-09-26 00:18:13 +0200

    add link to changelog in status

--- cogs/status.py
+++ cogs/status.py
@@ -23,7 +23,7 @@ class statuscmd(commands.Cog):
         
         
         embed = discord.Embed(title='PERSONA 3 RELOAD COUNTDOWN#8145', description=f'requested by <@{interaction.user.id}>', timestamp=datetime.datetime.now(), color=succesColor)
-        embed.add_field(name='Bot created by: nanewtech#0', value=f'**Bot Version:** v1.2.3 - Updated <t:1695656608:R>\n**Python Version:** 3.9.13\n**discord.py Version:** 2.1.0\n**Memory Usage:** {usage} MB\n**Active servers:** {len(self.bot.guilds)}\n**Current Session online since:** <t:{self.startupTIME}:f>')
+        embed.add_field(name='Bot created by: nanewtech#0', value=f'**Bot Version:** v1.5 - [changelog](https://github.com/nanewtech/P3Reload-Countdown-Bot/releases) - Updated <t:1695656608:R>\n**Python Version:** 3.9.13\n**discord.py Version:** 2.1.0\n**Memory Usage:** {usage} MB\n**Active servers:** {len(self.bot.guilds)}\n**Current Session online since:** <t:{self.startupTIME}:f>')
         embed.set_thumbnail(url=botPfp)
         embed.set_footer(text=f'PERSONA 3 RELOAD COUNTDOWN BOT', icon_url=botPfp)
         
